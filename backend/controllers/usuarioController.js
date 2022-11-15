40d1acfe44ad4b4065e0b5a18e05b1293fa14a95
ijcastrosena <111159778+ijcastrosena@users.noreply.github.com>
2022-11-14 21:12:54 -0500

    ajustes 14112022

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -43,6 +43,7 @@ const registrar = async (req, res)=>{
 const confirmar = async (req, res)=>{
     // req.params para leer datos de la URL, en este caso token por que asi lo definimos en la ruta
     const { token } = req.params;
+    // console.log(req.params);
     const usuarioConfirmar = await Usuario.findOne({token});
     // console.log(usuarioConfirmar);
     // console.log(token);
